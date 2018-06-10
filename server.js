f41eee3ade7082a447a818de7a7f61c930ee9b07
528491 <jacobminasian@gmail.com>
2018-06-09 21:39:30 -0700

    Ready to push to heroku for initial test.

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.port || 3000;
+
+app.get("/test", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/assets/test.html"));
+});
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
