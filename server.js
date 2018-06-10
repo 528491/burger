a79ac1186df05b80e23ef61a07b203f8a543841c
528491 <jacobminasian@gmail.com>
2018-06-09 21:42:57 -0700

    Fixed an error, port was mispelled.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 
 app.get("/test", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/assets/test.html"));
